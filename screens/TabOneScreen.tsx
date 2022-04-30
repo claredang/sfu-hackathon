24290c1f8ec40243cd7eab3b4122bdc95c0339b2
Clare Dang <claredang@Clares-MBP-2.vc.shawcable.net>
2022-04-30 01:01:07 -0700

    add sty;es

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,10 +7,12 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-      <Text>Test something here</Text>
+      <Text style={styles.title}>Corona Game</Text>
+      <Text>How to play</Text>
+      <Text style={styles.box1}>Choose a player to go first. That player throws a die and scores as many points as the total shown on the die providing the die doesn’t roll a 1. The player may continue rolling and accumulating points (but risk rolling a 1) or end his turn.</Text>
+      <Text style={styles.box}>If the player rolls a 1 his turn is over, he loses half points he accumulated that turn, and he passes the die to the next player</Text>
+      <Text>The first player to accumulate 100 or more points wins the game.</Text>
+      {/* <Button>Start the game</Button> */}
     </View>
   );
 }
@@ -20,14 +22,20 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    // backgroundColor: 'rgb(228,237,222)',
+    // backgroundColor: 'grey',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: 'black',
   },
   separator: {
-    marginVertical: 30,
+    // marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  box: {
+    backgroundColor: 'green',
+  }
 });

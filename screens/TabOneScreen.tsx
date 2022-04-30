6ec39630bd5404bee3bbfb9a4d913d381eb268cd
Clare Dang <claredang@Clares-MBP-2.vc.shawcable.net>
2022-04-29 21:59:16 -0700

    init

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -10,6 +10,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text>Test something here</Text>
     </View>
   );
 }
